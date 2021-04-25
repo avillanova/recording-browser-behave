26772880a3e2153a686e43a44a104c6ce205258c
alex.villanova <alex.villanova@mirumagency.com>
2021-04-24 22:46:42 -0300

    remove setup from gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='recording-browser-behave',
+    version='0.0.1',
+    description='To recording the browser in Thread',
+    author='Alex Villanova',
+    license='MIT',
+    packages=find_packages(include=['gen'])
+)
