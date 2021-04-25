ef706cfbbf6218767318d27f0007ffe73b0af077
alex.villanova <alex.villanova@mirumagency.com>
2021-04-24 22:55:12 -0300

    fix build

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 setup(
-    name='recording-browser-behave',
+    name='rec-browser-behave',
     version='0.0.1',
     description='To recording the browser in Thread',
     author='Alex Villanova',
     license='MIT',
-    packages=find_packages(include=['gen'])
+    packages=find_packages(include=['video-recorder'])
 )
