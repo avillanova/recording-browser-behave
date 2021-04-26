6e98c78790999d976b15a9c3bc3d6abd34f39b13
alex.villanova <alex.villanova@mirumagency.com>
2021-04-26 18:40:03 -0300

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version='0.1.0',
     description='To recording the browser in Thread',
     author='Alex Villanova',
+    author_email='alexsrvillanova@gmail.com',
     license='MIT',
     packages=find_packages(include=['video_recorder'])
 )
