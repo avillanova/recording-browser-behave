fe54a3f0812688a8b80f3443f796ee11d3a4f997
alex.villanova <alex.villanova@mirumagency.com>
2021-04-28 12:42:55 -0300

    add mkdir file

--- video_recorder/record_video.py
+++ video_recorder/record_video.py
@@ -9,34 +9,44 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import pkg_resources
+import logging
 
-path = 'Roboto-Black.ttf'  # always use slash
+path = 'Roboto-Black.ttf'
 filepath = pkg_resources.resource_filename(__name__, path)
-
+log = logging.getLogger('VideoRecorder')
 
 class Video(threading.Thread):
     def __init__(self, driver, video_name='evidence.mp4', four_cc='mp4v', fps=3, context=None,
                  color_hex='#000000', font=filepath,
                  alpha=50, show_url=False, show_step=False, font_size=32):
-
-        pathlib.Path(f'./{video_name}').mkdir(parents=True, exist_ok=True, mode=755)
+        pathlib.Path(video_name).mkdir(parents=True, exist_ok=True, mode=755)
         self.driver = driver
         self.video_name = video_name
         self.four_cc = four_cc
         self.context = context
-        self.height = self.width = 0
+        self.height = self.width = 1
         self.font = ImageFont.truetype(font, font_size)
         self.color_hex = color_hex
         self.show_url = show_url
         self.show_step = show_step
         self.alpha = alpha
         self.fps = fps
+        log.info(f'video_name: {self.video_name}')
+        log.info(f'four_cc: {self.four_cc}')
+        log.info(f'height and width: {self.height} {self.width}')
+        log.info(f'font: {font}')
+        log.info(f'font_size: {font_size}')
+        log.info(f'color_hex: {self.color_hex}')
+        log.info(f'show_url: {self.show_url}')
+        log.info(f'show_step: {self.show_step}')
+        log.info(f'alpha: {self.alpha}')
+        log.info(f'fps: {self.fps}')
         super(Video, self).__init__()
 
     def run(self):
         self.height, self.width, layers = self._get_matrix().shape
-        video = cv2.VideoWriter(self.video_name, cv2.VideoWriter_fourcc(*self.four_cc), self.fps,
-                                (self.width, self.height))
+        video = cv2.VideoWriter(
+            self.video_name, cv2.VideoWriter_fourcc(*self.four_cc), self.fps, (self.width, self.height))
         while True:
             try:
                 video.write(self._get_matrix())
