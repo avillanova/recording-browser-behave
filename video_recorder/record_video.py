0f6ba621b0d07262ff94d9d8e79cd3b8ce896bbe
alex.villanova <alex.villanova@mirumagency.com>
2021-04-27 21:38:30 -0300

    fix font

--- video_recorder/record_video.py
+++ video_recorder/record_video.py
@@ -7,14 +7,14 @@ import numpy as np
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import pkgutil
+import pkg_resources
 
-data = pkgutil.get_data(__name__, "Roboto-Black.ttf")
-
+path = 'Roboto-Black.ttf'  # always use slash
+filepath = pkg_resources.resource_filename(__name__, path)
 
 class Video(threading.Thread):
     def __init__(self, driver, video_name='evidence.mp4', four_cc='mp4v', fps=3, context=None,
-                 color_hex='#000000', font=data,
+                 color_hex='#000000', font=filepath,
                  alpha=50, show_url=False, show_step=False):
         print(font)
         print(data)
